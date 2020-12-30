187dcdbf1a54b2b49749cc87d866c10fce2d85bf
José Carlos López Gaona <josel@fwscience.com>
2020-12-30 11:39:55 -0600

    Initial iteration of filter/building lines for BalanceOutput

--- src/components/BalanceOutput.js
+++ src/components/BalanceOutput.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
+import {connect} from 'react-redux';
 import * as utils from '../utils';
 
 class BalanceOutput extends Component {
@@ -13,7 +13,7 @@ class BalanceOutput extends Component {
       <div className='output'>
         <p>
           Total Debit: {this.props.totalDebit} Total Credit: {this.props.totalCredit}
-          <br />
+          <br/>
           Balance from account {this.props.userInput.startAccount || '*'}
           {' '}
           to {this.props.userInput.endAccount || '*'}
@@ -28,24 +28,24 @@ class BalanceOutput extends Component {
         {this.props.userInput.format === 'HTML' ? (
           <table className="table">
             <thead>
-              <tr>
-                <th>ACCOUNT</th>
-                <th>DESCRIPTION</th>
-                <th>DEBIT</th>
-                <th>CREDIT</th>
-                <th>BALANCE</th>
-              </tr>
+            <tr>
+              <th>ACCOUNT</th>
+              <th>DESCRIPTION</th>
+              <th>DEBIT</th>
+              <th>CREDIT</th>
+              <th>BALANCE</th>
+            </tr>
             </thead>
             <tbody>
-              {this.props.balance.map((entry, i) => (
-                <tr key={i}>
-                  <th scope="row">{entry.ACCOUNT}</th>
-                  <td>{entry.DESCRIPTION}</td>
-                  <td>{entry.DEBIT}</td>
-                  <td>{entry.CREDIT}</td>
-                  <td>{entry.BALANCE}</td>
-                </tr>
-              ))}
+            {this.props.balance.map((entry, i) => (
+              <tr key={i}>
+                <th scope="row">{entry.ACCOUNT}</th>
+                <td>{entry.DESCRIPTION}</td>
+                <td>{entry.DEBIT}</td>
+                <td>{entry.CREDIT}</td>
+                <td>{entry.BALANCE}</td>
+              </tr>
+            ))}
             </tbody>
           </table>
         ) : null}
@@ -75,10 +75,74 @@ BalanceOutput.propTypes = {
   }).isRequired
 };
 
+/**
+ * Determines whether a value is between a given range.
+ *
+ * When one of the range delimiters is NaN:
+ *  - If it's the lower bound, value must be equal/lower than upper bound
+ *  - If it's the upper bound, valuet must be equal/greater than lower bound
+ *
+ * @param {number|NaN} start
+ * @param {number|NaN} end
+ * @param {number} value
+ */
+function valueBetweenRange(start, end, value) {
+  // Ranges are * *, so any accountNumber is valid
+  if (isNaN(start) && isNaN(end)) {
+    return true;
+  }
+
+  // Ranges are (*, number) or (number, *)
+  if (isNaN(start) || isNaN(end)) {
+    return isNaN(start) ? value <= end : value >= start;
+  }
+
+  return value >= start && value <= end;
+}
+
 export default connect(state => {
   let balance = [];
 
-  /* YOUR CODE GOES HERE */
+  const {accounts, journalEntries, userInput: {startPeriod, endPeriod, startAccount, endAccount, format}} = state;
+  // Filter accounts
+  const filteredAccounts = accounts
+    .filter((instance) => valueBetweenRange(startAccount, endAccount, instance.ACCOUNT))
+
+  // Extract numbers only for easy filtering of journal entries
+  const filteredAccountsKeys = filteredAccounts.map((instance) => instance.ACCOUNT)
+
+  // Filter/sort journals
+  const validJournalEntries = journalEntries.filter((instance) => {
+    // Periods must be defined in order to filter
+    if (!startPeriod || !endPeriod) {
+      return;
+    }
+
+    // Check if the entry belongs to a relevant account
+    if (filteredAccountsKeys.indexOf(instance.ACCOUNT) < 0) {
+      return;
+    }
+
+    return valueBetweenRange(startPeriod.getTime(), endPeriod.getTime(), instance.PERIOD)
+  })
+
+  balance = filteredAccounts.map((account) => {
+    const line = {
+      ACCOUNT: account.ACCOUNT,
+      DESCRIPTION: account.LABEL,
+      DEBIT: 0,
+      CREDIT: 0,
+      BALANCE: 0
+    };
+    return validJournalEntries
+      .filter((entry) => entry.ACCOUNT === account.ACCOUNT)
+      .reduce((acc, entry) => {
+        acc.DEBIT += entry.DEBIT
+        acc.CREDIT += entry.CREDIT
+        acc.BALANCE = acc.DEBIT - acc.CREDIT
+        return acc
+      }, line)
+  }).filter((line) => line.DEBIT !== 0 || line.CREDIT !== 0)
 
   const totalCredit = balance.reduce((acc, entry) => acc + entry.CREDIT, 0);
   const totalDebit = balance.reduce((acc, entry) => acc + entry.DEBIT, 0);
